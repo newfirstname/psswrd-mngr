12689227dc3d3dd68ed9ea527adcd8b3df2f4481
hamid814 <hamid331994@gmail.com>
2020-09-03 17:05:54 +0430

    dev

--- routes/privates.js
+++ routes/privates.js
@@ -10,13 +10,19 @@ router.get('/9560F74C21930B7CAA3634D647F52E6F', async (req, res, next) => {
   res.render('passes', {
     passwords: passes,
     count,
+    title: 'all',
   });
 });
 
 router.get('/33760E8BAFCFCFC68764091CF96EC423', async (req, res, next) => {
   const passes = await UserName.find({ final: true });
+  const count = await UserName.count({ final: true });
 
-  res.json(passes);
+  res.render('passes', {
+    passwords: passes,
+    count,
+    title: 'finals',
+  });
 });
 
 module.exports = router;
