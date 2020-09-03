2e8d892c2c415ce112edeba2f533696512df9fee
hamid814 <hamid331994@gmail.com>
2020-09-03 15:46:13 +0430

    dev

--- routes/privates.js
+++ routes/privates.js
@@ -3,4 +3,10 @@ const UserName = require('../models/UserName.js');
 
 const router = express.Router();
 
+router.get('/passes', async (req, res, next) => {
+  const passes = await UserName.find();
+
+  res.json(passes);
+});
+
 module.exports = router;
