8365b827aa07645a9eb038bd98e5b0823ec27aaf
hamid814 <hamid331994@gmail.com>
2020-09-07 18:30:42 +0430

    dev

--- client/src/components/PasswrodItem.js
+++ client/src/components/PasswrodItem.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import { Message } from 'rsuite';
 import { Panel } from 'rsuite';
 
 const PasswrodItem = ({ password }) => {
   return (
     <div>
       <Panel header={password.title} bordered>
-        {password.password}
+        {password.description && (
+          <Message type="warning" description={password.description} />
+        )}
+        <Message description={password.password} />
       </Panel>
     </div>
   );
